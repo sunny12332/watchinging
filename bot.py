e68f7599d69581742063dd6697bb3e2e485029d8
sunny12332 <sunny21972@gmail.com>
2021-08-05 10:38:42 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
-hello
+import discord
+from discord.ext import commands
+
+bot=commands.bot(command_prefix='.')
+
+@bot.event
+async def on_ready():
+    print(">>i am online<<")
+
+bot.run("ODcyNjI1MDYzOTQ4NTkxMTM0.YQslaw.0j8r3hfYAr-rWZ0PjLXbky7D-64")
+
+
+
+
