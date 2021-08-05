87c71ae7d56a321ff09242a6551238e63e717653
sunny12332 <sunny21972@gmail.com>
2021-08-05 10:54:22 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,12 @@ bot=commands.Bot(command_prefix='[')
 async def on_ready():
     print(">>i am online<<")
 
+@bot.event
+async def on_member_join(member):
+   print(f'{member} join!')
+   
+
+
 bot.run("ODcyNjI1MDYzOTQ4NTkxMTM0.YQslaw.0j8r3hfYAr-rWZ0PjLXbky7D-64")
 
 
